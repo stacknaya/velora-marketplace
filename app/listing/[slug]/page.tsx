738ac5459816dcd1256eebf7a968ea7f6b527449
stacknaya <hello@stackaiadvisor.com>
2026-09-06 13:11:49 -0500

    Reduce listing hero image width

--- app/listing/[slug]/page.tsx
+++ app/listing/[slug]/page.tsx
@@ -122,7 +122,7 @@ export default async function ListingPage({
           </div>
 
           {/* Hero image */}
-          <div className="overflow-hidden rounded-[2.4rem] border border-[#172033]/10 bg-white shadow-sm">
+          <div className="mx-auto max-w-[1200px] overflow-hidden rounded-[2.4rem] border border-[#172033]/10 bg-neutral-200">
             <div className="relative h-[420px] bg-neutral-200">
               {image ? (
                 <img
