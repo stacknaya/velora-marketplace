73c9d1faa66f8302c877522065ce49d9330dafc6
stacknaya <hello@stackaiadvisor.com>
2026-09-20 22:55:35 -0500

    Fix booking form state

--- components/BookingForm.tsx
+++ components/BookingForm.tsx
@@ -21,7 +21,8 @@ export default function BookingForm({
   }[];
   basePrice: number;
   priceUnit: string;
-}) {  const [showReview, setShowReview] = useState(false);
+}) {  const [range, setRange] = useState<DateRange | undefined>();
+  const [showReview, setShowReview] = useState(false);
 const [termsAccepted, setTermsAccepted] = useState(false);
 
   const earliestBookingDate = new Date();
